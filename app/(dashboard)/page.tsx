0118d64cfe2f63bf4a7ce45b158dbc509aea5526
RISHII7 <rishikesh.palande07@gmail.com>
2024-08-03 15:09:46 +0530

    Hono.js Setup for api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs"
+
 const DashboardPage = () => {
   return (
     <div>
         This is an authenticated Route..
+        <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
