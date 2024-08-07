062f669a352f2fa9e8f79cf0d057291528495661
RISHII7 <rishikesh.palande07@gmail.com>
2024-08-07 15:34:46 +0530

    Transactions hooks

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -134,6 +134,38 @@ const app = new Hono()
         },
     )
     .post(
+        "/bulk-create",
+        clerkMiddleware(),
+        zValidator(
+            "json",
+            z.array(
+                insertTransactionSchema.omit({
+                    id: true,
+                }),
+            ),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            };
+
+            const data = await db
+                .insert(transactions)
+                .values(
+                    values.map((value) => ({
+                        id: createId(),
+                        ...value,
+                    }))
+                )
+                .returning();
+
+            return c.json({ data });
+        },
+    )
+    .post(
         "/bulk-delete",
         clerkMiddleware(),
         zValidator(
@@ -211,7 +243,7 @@ const app = new Hono()
                     inArray(transactions.id, sql`(select id from ${transactionsToUpdate})`)
                 )
                 .returning();
-                
+
 
             if (!data) {
                 return c.json({ error: "Not found" }, 404);
